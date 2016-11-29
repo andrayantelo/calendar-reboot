351ac5dbd169afe3cd4160ef95479f1fab8f85fa
andrayantelo <andrayantelo@gmail.com>
2016-11-29 13:14:08 -0800

    added a few jquery enhancements

--- calendar.js
+++ calendar.js
@@ -1,5 +1,43 @@
 $(document).ready(function() {
     
+    //have the input textbox be same size as placeholder
+    // SO IN THIS CASE. THE THIS REFERS TO... THE OBJECT THAT INVOKES THE FUNCTION?
+    function sizePlaceholder() {
+        $(this).attr('size', $(this).attr('placeholder').length);
+        console.log($(this).prop("id") + " this is the id of this");
+    };
     
+ 
+    //have title resize when page is loaded, and when keys are pressed and released
+    $('#calendarTitle')
+        // event handler
+        .keyup(sizePlaceholder)
+        // resize on page load
+        .each(sizePlaceholder);
     
+    //take focus off calender title input form after enter is pressed
+    $('input[type=text]').keydown(function(e) {
+        //get the next index of text input element
+        var next_index = $('input[type=text]').index(this) + 1;
+        
+        //get number of text input element in an html document
+        var total_index = $('body').find('input[type=text]').length;
+        
+        //enter button in ASCII code
+        if (e.keyCode == 13) {
+            if (total_index == next_index) {
+                //go to the first text element if focused on the last
+                $('input[type=text]:eq(0)').focus();
+            }
+            else {
+             //go to the next text input element
+             $('input[type=text]:eq(' + next_index + ')').focus();
+         }
+        }
+    });
+    
+
+        
+   
+   
 });
