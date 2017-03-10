b1ec26e6778cc459ec320aed8e018a629783a793
andrayantelo <andrayantelo@gmail.com>
2017-03-10 12:04:38 -0800

    preparing module

--- tests/tests.js
+++ tests/tests.js
@@ -711,17 +711,11 @@ QUnit.test("validateForm test", function(assert) {
 });
 
 QUnit.module( "CheckIt tests for functions that involve store", {
-  before: function() {
-    // prepare something once for all tests
-  },
   beforeEach: function() {
     // prepare something before each test
   },
   afterEach: function() {
     // clean up after each test
-  },
-  after: function() {
-    // clean up once after all tests are done
   }
 });
 
